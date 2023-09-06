2fade8aff3a53f9b4c9e1dee95ebe51f08bfe438
Hossam Othman <rowheadrex@gmail.com>
2023-09-06 21:51:47 +0200

    promise all + fetch

--- traversyAsync/async.js
+++ traversyAsync/async.js
@@ -70,7 +70,8 @@ const promise2 = 10;
 const promise3 = new Promise((resolve, reject) => 
     setTimeout(resolve, 8000, 'Goodbye!')
     );
-Promise.all([promise1, promise2, promise3]).then(values => console.log(values));
+const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
+Promise.all([promise1, promise2, promise3, promise4]).then(values => console.log(values));
 
 
 ////////////////////////////////////////
