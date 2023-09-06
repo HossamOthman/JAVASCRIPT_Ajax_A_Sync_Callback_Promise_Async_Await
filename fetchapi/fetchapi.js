af6279a5df2736506a85086002c25a650a31d6e8
Hossam Othman <rowheadrex@gmail.com>
2023-09-06 19:35:53 +0200

    post to API final

--- fetchapi/fetchapi.js
+++ fetchapi/fetchapi.js
@@ -59,3 +59,34 @@ function getAPI() {
         document.getElementById("target1").innerHTML = output;
       });
 }
+////////////////////////////////
+// Post
+document.getElementById("addPost").addEventListener("submit", addPost);
+
+function addPost(e) {
+    e.preventDefault();
+    let title = document.getElementById('title').value;
+    let body = document.getElementById('body').value;
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-type': 'application/json'
+        },
+        body:JSON.stringify({title:title, body:body}) 
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
+        let output = "<h3>Data to be posted to the API server</h3>";
+        
+            output += `<div class="posts">
+                              <h4>Title: ${data.title}</h4>
+                              <p>Body: ${data.body}</p>
+  
+                      </div>`;
+          
+          document.getElementById("target2").innerHTML = output;
+    })
+}
