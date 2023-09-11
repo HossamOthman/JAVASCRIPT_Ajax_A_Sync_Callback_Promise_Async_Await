255e2b89f40b8d10aa564ba39dadf12a179c6252
Hossam Othman <rowheadrex@gmail.com>
2023-09-11 12:20:48 +0200

    Search App Done

---
+++ searchapp/asyncawait.js
@@ -0,0 +1,45 @@
+const search = document.getElementById('search');
+const matchList = document.getElementById('match-list');
+
+
+
+search.addEventListener('input', () => searchStates(search.value));
+
+
+// search the states_cities.json and filter it
+// ...fetching the json using async await
+const searchStates = async searchText => {
+    const res = await fetch('../samples/states_cities.json');
+    const states = await res.json();
+
+    // Get matches to current search input
+    let matches = states.filter(state => {
+        // create a regular expression to match
+        const regex = new RegExp(`^${searchText}`, 'gi');
+        
+        // return an array of these matches
+        return state.state.match(regex) || state.name.match(regex);
+    });
+    if (searchText.length == 0) {
+        matches = [];
+    }
+    
+    // we got the Data now we stich it in the html! 
+    outputHtml(matches);
+};
+
+const outputHtml = matches => {
+    if (matches.length > 0) {
+        const html = matches.map(match => `
+        <div class="card">
+            <h3>${match.name}</h3>
+            <h4>State: <span> ${match.state}</span> - District: <span>${match.district}</span></h4>
+            <small>Area: ${match.area} | Population: ${match.population} | Latitude: ${match.coords.lat} / Longitude: ${match.coords.lon}</small>
+        </div>
+        ` ).join('');
+
+        matchList.innerHTML = html;
+    }
+}
+
+
